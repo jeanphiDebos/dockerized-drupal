a48cb3e477d9c0d74222adc4bc903b7145f9c708
Mathieu Wambre <mathieu.wambre@niji.fr>
2018-10-08 08:28:05 +0200

    Remove clean warnings

--- scripts/clean-workspace.php
+++ scripts/clean-workspace.php
@@ -1,10 +1,7 @@
 #!/usr/bin/php
 <?php
 rename('composer.dist.json', 'composer.json');
-unlink('composer.lock');
-unlink('scripts/clean-workspace.php');
-unlink('scripts/generate-env-file.php');
-
-if (count(scandir('./scripts')) === 2) { // ['.', '..']
-  rmdir('./scripts');
-}
+@unlink('composer.lock');
+@unlink('scripts/clean-workspace.php');
+@unlink('scripts/generate-env-file.php');
+@rmdir('./scripts');
