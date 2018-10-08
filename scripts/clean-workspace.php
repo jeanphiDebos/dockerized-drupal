1e2d26928e6783c3502861855340abeb78ee7f8e
Mathieu Wambre <mathieu.wambre@niji.fr>
2018-10-08 07:08:53 +0200

    Composer use php scripts

--- scripts/clean-workspace.php
+++ scripts/clean-workspace.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
-/*rename('composer.dist.json', 'composer.json');
+rename('composer.dist.json', 'composer.json');
 unlink('composer.lock');
 unlink('scripts/clean-workspace.php');
-unlink('scripts/generate-env-file.php');*/
+unlink('scripts/generate-env-file.php');
 
 if (count(scandir('./scripts')) === 2) { // ['.', '..']
   rmdir('./scripts');
